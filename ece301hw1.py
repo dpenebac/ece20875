7ea1003cfb04d35e378d375462925675b83a8bde
dpenebac <dpenebac@purdue.edu>
2022-01-25 10:55:20 -0500

    comment for signals and systems hw

---
+++ ece301hw1.py
@@ -0,0 +1,9 @@
+'''
+t = linspace(-pi, pi)
+omega = 2 * pi / 7
+fifth = (sine wave with frequence 5 * omega * t)
+k = -2
+negative_second = (a sine wave witha frequencey -2 * omega * t)
+plot(t, fifth)
+plot(t, negative_second) (plot on same graph)
+'''
